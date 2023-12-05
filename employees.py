c826b9167dde6e52dbac963e35ba1f236a9dea99
abdewan <abdewan@umich.edu>
2023-12-05 17:37:36 -0500

    finished the employees database, need to figure out the serpapi stuff

--- employees.py
+++ employees.py
@@ -6,7 +6,19 @@ import re
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import requests
+#from serpapi import GoogleSearch
 
+
+#scrapes the url and returns the html object
+def scrapeUrl(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        html = response.text
+    else:
+        print("Failed to retrieve the web page.")
+    return html
+
+#creates a database with the passed name
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
@@ -53,7 +65,7 @@ def tableSetUp(html, additional):
 
     return topPaid
 
-
+#adds the employee data in the top 100 list into the database
 def addToDatabase(cur, conn, top100):
     cur.execute("DROP TABLE IF EXISTS employees")
     cur.execute("CREATE TABLE IF NOT EXISTS employees (id INTEGER PRIMARY KEY, name TEXT, department TEXT, title TEXT, salary INTEGER)")
@@ -64,18 +76,28 @@ def addToDatabase(cur, conn, top100):
         id += 1
     conn.commit()
 
+def getScholarIDs(top100):
+    allProfiles = []
+    for person in top100:
+        params = {
+            "engine": "google_scholar_profiles",
+            "mauthors": person[1],
+            "api_key": "cb4afefcf1639be4471e44507bafaeb29e7035b1ae95037c8f65603ab631ea08"
+        }
+
+        search = google_search(params)
+        results = search.get_dict()
+        profiles = results["profiles"]
+        allProfiles.append(profiles)
+
+def createScholarTable(cur, conn, top100):
+    pass
+
     
         
 def main():
-    url = 'https://www.umsalary.info/numbers.php'
 
-    # browser = webdriver.Chrome(options=options)
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        html = response.text
-    else:
-        print("Failed to retrieve the web page.")
+    html = scrapeUrl('https://www.umsalary.info/numbers.php')
 
     #need to add the 75 other employees to this list
     additional = ['Luanne Ewald']
@@ -84,6 +106,11 @@ def main():
 
     cur, conn = setUpDatabase('employeePay')
     addToDatabase(cur, conn, top100)
+
+    getScholarIDs(top100)
+
+    # cur2, conn2 = setUpDatabase('googleScholars')
+    # createScholarTable(cur2, conn2, top100)
     
 
 
