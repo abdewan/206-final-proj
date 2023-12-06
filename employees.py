636e377bd7402543a11c0eac7cc4ad419064b440
abdewan <abdewan@umich.edu>
2023-12-06 00:00:32 -0500

    set up the top paid professor table fully

--- employees.py
+++ employees.py
@@ -19,12 +19,15 @@ results = serpapi.search(params).as_dict()
 
 #scrapes the url and returns the html object
 def scrapeUrl(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        html = response.text
-    else:
-        print("Failed to retrieve the web page.")
-    return html
+    htmls = []
+    for i in range(4):
+        params={'pg' : i}
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            htmls.append(response.text)
+        else:
+            print("Failed to retrieve the web page.")
+    return htmls
 
 #creates a database with the passed name
 def setUpDatabase(db_name):
@@ -45,43 +48,21 @@ def findTags(html, tag):
     return cells
 
 #Sets up the table of top 100 highest paid employees and returns a list of tuples
-def tableSetUp(html):
-    cells = findTags(html, "td")
+def tableSetUp(htmls):
     topPaid = []
-    #looping through the section of list once the employees start (at index 6), breaking into tuples to append to my list
-    for i in range(0, len(cells)-6, 6):
-        name = cells[i+2]
-        #reordering the names to be first middle last 
-        match = re.search('(\w+)\s(\D+)', name)
-        name = match.group(1) + ' ' + match.group(2)
-        title = cells[i+3]
-        #converting salaries into ints
-        salary = int(cells[i+4][1:-3].replace(',',''))
-        if 'Professor' in title:
-            topPaid.append((name, title, salary))
-
-
-    #for i in range(0, len(cells) - 2):
-        #values = cells[i].split('\n')
-        #converting salaries into ints
-        #values[4] = int(values[4][2:-3].replace(',', ''))
-        #(name, title, department, salary)
-        #topPaid.append((values[1], values[2], values[3], values[4]))
-
-    url = 'https://www.umsalary.info/index.php?FName=Luanne&LName=Ewald&Year=0'
-    #searching for each additional employee in the top 100 on the website and extracting their information to add to our database
-    # for person in additional:
-    #     name = person.split(' ')
-    #     params = {'FName': name[0], 'LName': name[1], 'Year': 0}
-    #     response = requests.get(url, params=params)
-    #     if response.status_code == 200:
-    #         html2 = response.text
-    #     else:
-    #         print("Failed to retrieve the web page.")
-
-    #     cells2 = findTags(html2, "td")
-    #     cells2[13] = int(cells2[13][2:-3].replace(',', ''))
-    #     topPaid.append((cells2[10], cells2[11], cells2[12], cells2[13]))
+    for html in htmls:
+        cells = findTags(html, "td")
+        #looping through the section of list once the employees start (at index 6), breaking into tuples to append to my list
+        for i in range(0, len(cells)-6, 6):
+            name = cells[i+2]
+            #reordering the names to be first middle last 
+            match = re.search('(\w+)\s(\D+)', name)
+            name = match.group(1) + ' ' + match.group(2)
+            title = cells[i+3]
+            #converting salaries into ints
+            salary = int(cells[i+4][1:-3].replace(',',''))
+            if 'Professor' in title:
+                topPaid.append((name, title, salary))
 
     return topPaid
 
@@ -92,7 +73,7 @@ def addToDatabase(cur, conn, top100):
     conn.commit()
 
     id = 1
-    for employee in top100:                                                                                                                                                                                      
+    for employee in top100:                                                                                                                                                                                
         cur.execute('INSERT INTO professors (id, name, title, salary) VALUES (?,?,?,?)', (id, employee[0], employee[1], employee[2]))
         id += 1
     conn.commit()
@@ -118,11 +99,11 @@ def createScholarTable(cur, conn, top100):
         
 def main():
 
-    html = scrapeUrl('https://www.openthebooks.com/michigan-state-employees/?Year_S=2022&Emp_S=University%2Bof%2BMichigan%2Bat%2BAnn%2BArbor&pg=1')
+    htmls = scrapeUrl('https://www.openthebooks.com/michigan-state-employees/?Year_S=2022&Emp_S=University%2Bof%2BMichigan%2Bat%2BAnn%2BArbor')
 
-    top100 = tableSetUp(html)
+    top100 = tableSetUp(htmls)
 
-    cur, conn = setUpDatabase('professorPay')
+    cur, conn = setUpDatabase('professorPay.db')
     addToDatabase(cur, conn, top100)
 
     #getScholarIDs(top100)
