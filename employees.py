3648b8273d165c722f40d5ea235b4c799d2f37b6
abdewan <abdewan@umich.edu>
2023-12-08 16:34:18 -0500

    finished database setup

--- employees.py
+++ employees.py
@@ -60,31 +60,34 @@ def tableSetUp(htmls):
 
     return topPaid
 
-#adds the employee data in the top 100 list into the database
+#adds the employee data in the top 100 list into the database 25 rows at a time.
 def addToDatabase(cur, conn, top100):
     print(len(top100))
-    cur.execute("DROP TABLE IF EXISTS professorPay")
+    #cur.execute("DROP TABLE IF EXISTS professorPay")
     cur.execute("CREATE TABLE IF NOT EXISTS professorPay (id INTEGER PRIMARY KEY, name TEXT, title TEXT, salary INTEGER, scholar_id TEXT)")
     conn.commit()
     
-    # cur.execute('SELECT COUNT(*) FROM professorPay')
-    # result = cur.fetchone()
-    # print('num rows:', result[0])
-    # if (result[0] > 0):
-    #     cur.execute('SELECT id FROM professorPay')
-    #     results = cur.fetchall()
-    #     curIdx = results[-1][0]
-    #     print('current index:', curIdx)
-    #     for i in range(curIdx, curIdx + 25):
-    #         cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (i+1, top100[i][0], top100[i][1], top100[i][2], top100[i][3]))
-    # else:
-    #     for i in range(0, 25):
-    #         cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (i+1, top100[i][0], top100[i][1], top100[i][2], top100[i][3]))
-    id=1
-    for prof in top100:
-        cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (id, prof[0], prof[1], prof[2], prof[3]))
-        id += 1
-    conn.commit()
+    #get number of rows
+    cur.execute('SELECT COUNT(*) FROM professorPay')
+    result = cur.fetchone()
+    print('num rows:', result[0])
+    #check if empty
+    if (result[0] > 0):
+        cur.execute('SELECT id FROM professorPay')
+        results = cur.fetchall()
+        curIdx = results[-1][0]
+        print('current index:', curIdx)
+        for i in range(curIdx, curIdx + 25):
+            cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (i+1, top100[i][0], top100[i][1], top100[i][2], top100[i][3]))
+    else:
+        #insert for the first time
+        for i in range(0, 25):
+            cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (i+1, top100[i][0], top100[i][1], top100[i][2], top100[i][3]))
+    # id=1
+    # for prof in top100:
+    #     cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (id, prof[0], prof[1], prof[2], prof[3]))
+    #     id += 1
+    # conn.commit()
 
 
 #removes duplicates from the list
@@ -97,26 +100,6 @@ def removeDuplicates(top100):
             unique.append(prof[0])
     return top100
 
-#we are using this to use each professor's name to access their authorID which can then be used with the profiles API
-#because we only get a limited number of searches with this API, we are storing the search results for 100 professors in an authorIDs.json file
-#adding the pass keyword to the top of this function now that we have the saved .json file so we don't use up more free searches
-# def saveAuthorIDs(top100):
-
-#     authorIDs = []
-#     for professor in top100:
-#         name = professor[0]
-
-#         params = {
-#         "engine": "google_scholar_profiles",
-#         "mauthors": name,
-#         "api_key": "cc8983ecd018d044d8be6c698a56199750adaa55869c8d78b42fde703095769a"
-#         }
-
-#         results = serpapi.search(params).as_dict()
-#         authorIDs.append(results)
-
-#         with open('authorIDs.json', 'w') as json_file:
-#             json.dump(authorIDs, json_file)
 
 def saveAuthorIDs(top100):
 
@@ -140,40 +123,45 @@ def saveAuthorIDs(top100):
             result = next(query)
             print('appending', name, 'at index', len(authorIDs))
             authorIDs.append((name, professor[1], professor[2], result['scholar_id']))
-            #print('added', (name, professor[1], professor[2], result['scholar_id']))
         except:
             counter += 1
             print('removing', professor)
-            #top100.remove(professor)
-            #print('removed ', professor[0])
+            
         
         #print(result['scholar_id'])
         #authorIDs.append((name, professor[1], professor[2], 'n/a'))
     print('removed', counter)
-    #print(len(authorIDs))
     return authorIDs
     
 
 
 #same process as saveAuthorIDs() but different query, now saving their citations and such
-def saveCitations(authorIDs):
+def saveCitations(cur):
 
     citations = []
-    
-    for professor in authorIDs:
-        id = professor[0]
+
+    cur.execute('SELECT scholar_id FROM professorPay')
+    rows = cur.fetchall()
+    i=1
+    for prof in rows:
+        #can only do 100 rows
+        if i > 100:
+            break
+        id = prof[0]
 
         params = {
         "engine": "google_scholar_author",
         "author_id": id,
-        "api_key": "cc8983ecd018d044d8be6c698a56199750adaa55869c8d78b42fde703095769a"
+        "api_key": "48aae026c3b879c625d8a4fe9598c472f3c6a357cf8785f839e609c36d4418fd"
         }
 
         results = serpapi.search(params).as_dict()
+        print(i)
+        i+=1
         citations.append(results)
 
-        with open('citations.json', 'w') as json_file:
-            json.dump(citations, json_file)
+    with open('citations.json', 'w') as json_file:
+        json.dump(citations, json_file)
 
 def createCitationTable(cur, conn, filename):
     f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), filename)))
@@ -184,20 +172,28 @@ def createCitationTable(cur, conn, filename):
     citations = []
     for author in data:
         id = author['search_parameters']['author_id']
-        citations = author['cited_by']['table'][0]['all']
-        h_index = author['cited_by']['table'][0]['indice_h']
-        citations.append((id, citations, h_index))
+        numCitations = author['cited_by']['table'][0]['citations']['all']
+        h_index = author['cited_by']['table'][1]['h_index']['all']
+        citations.append((id, numCitations, h_index))
 
-    cur.execute('DROP TABLE IF EXISTS citations')
+    #cur.execute('DROP TABLE IF EXISTS citations')
     cur.execute('CREATE TABLE IF NOT EXISTS citations (authorID TEXT PRIMARY KEY, citations INTEGER, h_index INTEGER)')
     conn.commit()
 
-    for i in range(4):
-        for j in range(25):
-            cur.execute('INSERT INTO citations (authorID, citations, h_index) VALUES (?,?,?)', (id, citations[(25*i)+j][0], citations[(25*i)+j][1], citations[(25*i)+j][2]))                                                                                                                                                                      
-        conn.commit()
 
-    return citations
+    cur.execute('SELECT COUNT(*) FROM citations')
+    result = cur.fetchone()
+    print('num rows:', result[0])
+    #check if empty
+    curIdx = result[0]
+    if (curIdx > 0):
+        for i in range(curIdx, curIdx + 25):
+            cur.execute('INSERT INTO citations (authorID, citations, h_index) VALUES (?,?,?)', (citations[i][0], citations[i][1], citations[i][2]))
+    else:
+        #insert for the first time
+        for i in range(0, 25):
+            cur.execute('INSERT INTO citations (authorID, citations, h_index) VALUES (?,?,?)', (citations[i][0], citations[i][1], citations[i][2]))
+    conn.commit()
 
 
 def createScholarTable(cur, conn, top100):
@@ -217,16 +213,15 @@ def main():
     cur, conn = setUpDatabase('professors.db')
     print(len(profs))
     #commenting out because we have our complete database and don't need to keep scraping the url every time, it takes a while
-    top100 = saveAuthorIDs(profs)
-    addToDatabase(cur, conn, top100)
-    print(len(top100))
+    #top100 = saveAuthorIDs(profs)
+    #addToDatabase(cur, conn, top100)
 
     #authorIDs = scrapeAuthorIDs(url, top100)
     #print(authorIDs)
 
-    
-    # saveCitations(authorIDs)
-    # citations = createCitationTable(cur, conn, 'citations.json')
+    #commenting out so we don't keep running the api 
+    #saveCitations(cur)
+    createCitationTable(cur, conn, 'citations.json')
     
 
 
