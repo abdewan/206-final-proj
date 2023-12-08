5fcf9868cb4e717b747d9d5a6123b7c2b8ca1836
abdewan <abdewan@umich.edu>
2023-12-07 19:36:45 -0500

    google scholar blocked me

--- employees.py
+++ employees.py
@@ -7,8 +7,10 @@ import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import requests
 import serpapi
+from scholarly import scholarly
 
-
+# query = scholarly.search_author('Duderstadt, Michigan')
+# scholarly.pprint(next(query))
 #scrapes the url and returns the html object
 def scrapeUrl(url):
     htmls = []
@@ -60,16 +62,30 @@ def tableSetUp(htmls):
 
 #adds the employee data in the top 100 list into the database
 def addToDatabase(cur, conn, top100):
+    print(len(top100))
     cur.execute("DROP TABLE IF EXISTS professorPay")
-    cur.execute("CREATE TABLE IF NOT EXISTS professorPay (id INTEGER PRIMARY KEY, name TEXT, title TEXT, salary INTEGER)")
+    cur.execute("CREATE TABLE IF NOT EXISTS professorPay (id INTEGER PRIMARY KEY, name TEXT, title TEXT, salary INTEGER, scholar_id TEXT)")
+    conn.commit()
+    
+    # cur.execute('SELECT COUNT(*) FROM professorPay')
+    # result = cur.fetchone()
+    # print('num rows:', result[0])
+    # if (result[0] > 0):
+    #     cur.execute('SELECT id FROM professorPay')
+    #     results = cur.fetchall()
+    #     curIdx = results[-1][0]
+    #     print('current index:', curIdx)
+    #     for i in range(curIdx, curIdx + 25):
+    #         cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (i+1, top100[i][0], top100[i][1], top100[i][2], top100[i][3]))
+    # else:
+    #     for i in range(0, 25):
+    #         cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (i+1, top100[i][0], top100[i][1], top100[i][2], top100[i][3]))
+    id=1
+    for prof in top100:
+        cur.execute('INSERT INTO professorPay (id, name, title, salary, scholar_id) VALUES (?,?,?,?,?)', (id, prof[0], prof[1], prof[2], prof[3]))
+        id += 1
     conn.commit()
 
-    id = 1
-    for i in range(4):
-        for j in range(25):
-            cur.execute('INSERT INTO professorPay (id, name, title, salary) VALUES (?,?,?,?)', (id, top100[(25*i)+j][0], top100[(25*i)+j][1], top100[(25*i)+j][2]))                                                                                                                                                                      
-            id += 1
-        conn.commit()
 
 #removes duplicates from the list
 def removeDuplicates(top100):
@@ -85,7 +101,6 @@ def removeDuplicates(top100):
 #because we only get a limited number of searches with this API, we are storing the search results for 100 professors in an authorIDs.json file
 #adding the pass keyword to the top of this function now that we have the saved .json file so we don't use up more free searches
 # def saveAuthorIDs(top100):
-#     pass
 
 #     authorIDs = []
 #     for professor in top100:
@@ -94,7 +109,7 @@ def removeDuplicates(top100):
 #         params = {
 #         "engine": "google_scholar_profiles",
 #         "mauthors": name,
-#         "api_key": "cb4afefcf1639be4471e44507bafaeb29e7035b1ae95037c8f65603ab631ea08"
+#         "api_key": "cc8983ecd018d044d8be6c698a56199750adaa55869c8d78b42fde703095769a"
 #         }
 
 #         results = serpapi.search(params).as_dict()
@@ -103,42 +118,55 @@ def removeDuplicates(top100):
 #         with open('authorIDs.json', 'w') as json_file:
 #             json.dump(authorIDs, json_file)
 
-def createAuthorIDTable(cur, conn, filename):
-    f = open(os.path.abspath(os.path.join(os.path.dirname(__file__), filename)))
-    file_data = f.read()
-    f.close()
-    data = json.loads(file_data)
-    #data = data[1:]
+def saveAuthorIDs(top100):
 
     authorIDs = []
-    for author in data:
-        authorID = author['profiles'][0]['author_id']
-        name = author['profiles'][0]['name']
-        authorIDs.append((authorID, name))
-
-    cur.execute('DROP TABLE IF EXISTS authorIDs')
-    cur.execute('CREATE TABLE IF NOT EXISTS authorIDs (authorID TEXT PRIMARY KEY, name TEXT)')
-    conn.commit()
-
-    for i in range(4):
-        for j in range(25):
-            cur.execute('INSERT INTO authorIDs (authorID, name) VALUES (?,?)', (id, authorIDs[(25*i)+j][0], authorIDs[(25*i)+j][1]))                                                                                                                                                                      
-        conn.commit()
-
+    counter = 0
+    i=1
+    for professor in top100:
+        name = ''
+        temp = professor[0].split(' ')
+        if len(temp) > 2:
+            if temp[0] == 'Jr':
+                name = temp[1] + ' ' + temp[-1]
+            else:
+                name = temp[0] + ' ' + temp[-1]
+        else:
+            name = professor[0]
+        query = scholarly.search_author(f'{name}, Michigan')
+        i+=1
+        #scholarly.pprint(query)
+        try:
+            result = next(query)
+            print('appending', name, 'at index', len(authorIDs))
+            authorIDs.append((name, professor[1], professor[2], result['scholar_id']))
+            #print('added', (name, professor[1], professor[2], result['scholar_id']))
+        except:
+            counter += 1
+            print('removing', professor)
+            #top100.remove(professor)
+            #print('removed ', professor[0])
+        
+        #print(result['scholar_id'])
+        #authorIDs.append((name, professor[1], professor[2], 'n/a'))
+    print('removed', counter)
+    #print(len(authorIDs))
     return authorIDs
+    
+
 
 #same process as saveAuthorIDs() but different query, now saving their citations and such
 def saveCitations(authorIDs):
-    pass
 
     citations = []
+    
     for professor in authorIDs:
         id = professor[0]
 
         params = {
         "engine": "google_scholar_author",
         "author_id": id,
-        "api_key": "cb4afefcf1639be4471e44507bafaeb29e7035b1ae95037c8f65603ab631ea08"
+        "api_key": "cc8983ecd018d044d8be6c698a56199750adaa55869c8d78b42fde703095769a"
         }
 
         results = serpapi.search(params).as_dict()
@@ -181,19 +209,24 @@ def main():
 
     htmls = scrapeUrl('https://www.openthebooks.com/michigan-state-employees/?Year_S=2022&Emp_S=University%2Bof%2BMichigan%2Bat%2BAnn%2BArbor')
 
-    top100 = tableSetUp(htmls)
-    top100 = removeDuplicates(top100)
+    profs = tableSetUp(htmls)
+    profs = removeDuplicates(profs)
     #filter only top 100 from list 
-    top100 = top100[:100]
+    #top100 = top100[:100]
 
     cur, conn = setUpDatabase('professors.db')
+    print(len(profs))
+    #commenting out because we have our complete database and don't need to keep scraping the url every time, it takes a while
+    top100 = saveAuthorIDs(profs)
     addToDatabase(cur, conn, top100)
+    print(len(top100))
 
-    #saveAuthorIDs(top100)
-    #authorIDs = createAuthorIDTable(cur, conn, 'authorIDs.json')
+    #authorIDs = scrapeAuthorIDs(url, top100)
+    #print(authorIDs)
 
-    #saveCitations(authorIDs)
-    #citations = createCitationTable(cur, conn, 'citations.json')
+    
+    # saveCitations(authorIDs)
+    # citations = createCitationTable(cur, conn, 'citations.json')
     
 
 
